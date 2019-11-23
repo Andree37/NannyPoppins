32f8c6cca022896c2347b7b3a63b974fdedeba4e
Andre <andresmribeiro@gmail.com>
2019-11-23 19:05:46 +0000

    Logo and splash

--- NannyPoppins/app/src/main/java/com/example/nannypoppins/MainActivity.java
+++ NannyPoppins/app/src/main/java/com/example/nannypoppins/MainActivity.java
@@ -8,7 +8,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashTheme);
         super.onCreate(savedInstanceState);
+
+        //remove this afterwards
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
     }
 }
